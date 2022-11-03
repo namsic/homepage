999074f0f37651f088cd9785b01200a08c7c4f86
Namjae Kim <kimnj3050@gmail.com>
2022-11-03 11:38:00 +0900

    feat: Add cargo project (#1)
    * feat: Add cargo project
    * test: Fix testcase
    * fix: Fix typo

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(1 + 2, 3);
+    }
+}
